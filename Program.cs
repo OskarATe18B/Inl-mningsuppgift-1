5210ac5e48695c001ab1c6cb12ad4843d5407e65
OskarATe18B <54435174+OskarATe18B@users.noreply.github.com>
2019-09-13 09:49:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inlämningsuppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //generate a random number between 1-100
+            Random random = new Random();
+            int ranNum = random.Next(1, 100);
+            Console.WriteLine(ranNum);
+            
+        }
+    }
+}
